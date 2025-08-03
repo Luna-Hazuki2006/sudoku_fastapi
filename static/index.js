95421f30bddae1d0623b1a4f6ec538871f70501c
Luna Hazuki <lunahazuki2006@outlook.com>
2025-08-03 17:02:00 -0400

    Con cambiable sin bugs

--- static/index.js
+++ static/index.js
@@ -5,13 +5,15 @@ let casillas = document.getElementsByClassName('cambiable')
 for (const cada of casillas) {
     cada.onclick = () => {
         casilla = cada
-        casilla.innerHTML = ''
-        let input = document.createElement('input')
-        input.type = 'number'
-        input.min = '1'
-        input.max = '9'
-        input.step = '1'
-        casilla.appendChild(input)
+        if (['1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(cada.innerText)) {
+            casilla.innerHTML = ''
+            let input = document.createElement('input')
+            input.type = 'number'
+            input.min = '1'
+            input.max = '9'
+            input.step = '1'
+            casilla.appendChild(input)
+        }
         console.log(casilla);
     }
 }
